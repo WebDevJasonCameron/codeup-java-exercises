82d622dd7542324e2651b43c4be132b2b7a636c6
Jason Cameron <dev.jasoncameron@gmail.com>
2022-04-04 15:19:03 -0500

    added monsterturck for mini ex

--- src/vehicle/MonsterTruck.java
+++ src/vehicle/MonsterTruck.java
@@ -1,4 +1,17 @@
 package vehicle;
 
-public class MonsterTruck {
+public class MonsterTruck extends Vehicle {
+
+    // CON
+
+
+    public MonsterTruck(String name, String color) {
+        super(name, color);
+    }
+
+    // OVER
+    public void makeNoise() {
+        super.makeNoise();
+        System.out.println("ROAR!!!!!");
+    }
 }
--- src/vehicle/Vehicle.java
+++ src/vehicle/Vehicle.java
@@ -2,21 +2,33 @@ package vehicle;
 
 public class Vehicle {
     private String name;
+    private String color;
 
     // CON
-    public Vehicle(String name) {
+    public Vehicle(String name, String color) {
         this.name = name;
+        this.color = color;
     }
+    public Vehicle(){};
+
     // GETS
     public String getName() {
         return name;
     }
+    public String getColor() {
+        return color;
+    }
+
     // SETS
     public void setName(String name) {
         this.name = name;
     }
+    public void setColor(String color) {
+        this.name = color;
+    }
+
     // METHS
-    public static void makeNoice(){
+    public void makeNoise(){
         System.out.println("Vroom, Vroom");
     }
 
